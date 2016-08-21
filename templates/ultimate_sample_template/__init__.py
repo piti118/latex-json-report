faa963e94df5a4dbe41781d8f88fa0b18a7318c0
Piti <piti118@gmail.com>
2016-08-21 22:09:44 +0700

    requirement and template

--- templates/ultimate_sample_template/__init__.py
+++ templates/ultimate_sample_template/__init__.py
@@ -1,7 +1,12 @@
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 from template_base import TemplateBase
 from os import path
 import shutil
-__all__=['template']
+
+
+__all__ = ['template']
 
 def template():
     return UltimateSampleTemplate()
@@ -10,6 +15,14 @@ class UltimateSampleTemplate(TemplateBase):
     def __init__(self):
         super().__init__(path.dirname(__file__)) #this setup jinja loader
         pass
+
+    def make_graph(self, data, cwd):
+        plt.figure()
+        plt.plot(data['raw_numbers'])
+        plt.grid(True)
+        output = path.join(cwd, 'graph.pdf')
+        plt.savefig(output, bbox_inches='tight')
+
 
     def preprocess(self, data, cwd):
         """
@@ -20,10 +33,11 @@ class UltimateSampleTemplate(TemplateBase):
         """
         asset_dir = path.join(path.dirname(__file__), 'assets')
         shutil.copytree(asset_dir, path.join(cwd, 'assets')) #should we symlink instead?
+        self.make_graph(data, cwd)
         return data
 
     def get_template(self, data):
         return 'template.tex'
 
     def sample_data(self):
-        return { "name": "ปิติ" }
+        return { "name": "ปิติ", "raw_numbers":[2,4,-3,7] }
